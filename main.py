8fd2660f65b4a6cb76d62983d869cdf90c1acf5e
TheRealFezbot <ferryrietveld@live.nl>
2025-07-30 15:06:27 +0300

    D: added ability to turn player

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ def main():
     running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     # game loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
         
         screen.fill('black')
         player.draw(screen)
