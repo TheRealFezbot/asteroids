abccad5455eb3b3e57ea33709aa00ef763d286ed
TheRealFezbot <ferryrietveld@live.nl>
2025-07-30 14:59:35 +0300

    C: Added player class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     # start pygame
@@ -11,6 +11,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     # game loop
     while running:
@@ -19,6 +20,7 @@ def main():
                 return
         
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
