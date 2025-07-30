bd3a9e582605ba6308688dbcca7af157d57102d3
TheRealFezbot <ferryrietveld@live.nl>
2025-07-30 14:45:48 +0300

    B: added Delta time and CircleShape class

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ from constants import *
 def main():
     # start pygame
     pygame.init 
-    running = True  
 
-    # Welcome message
-    print('Starting Asteroids!')
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    # create main window
+    # setup    
+    running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     # game loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill('black')
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
+        
 
 if __name__ == "__main__":
     main()
